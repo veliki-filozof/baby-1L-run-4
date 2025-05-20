646c16804ba6bad9eb063b91537fefbde77e50a4
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-05-20 10:43:04 -0400

    fixed measured TBR calculation

--- analysis/tritium/tritium_model.py
+++ analysis/tritium/tritium_model.py
@@ -200,23 +200,29 @@ calculated_TBR_std_dev = (
 total_irradiation_time = sum([irr[1] - irr[0] for irr in irradiations])
 
 T_consumed = neutron_rate * total_irradiation_time
+
+# to calculate the measured TBR we ignore the last samples for which
+# we have some contribution from other sources (nGen, cyclotron, etc.)
+nb_samples_ignored = 5  # ignore the last 5 samples
 T_produced = sum(
-    [stream.get_cumulative_activity("total")[-1] for stream in run.streams]
+    [
+        stream.get_cumulative_activity("total")[-nb_samples_ignored - 1]
+        for stream in run.streams
+    ]
 )
 
 measured_TBR = (T_produced / quantity_to_activity(T_consumed)).to(
     ureg.particle * ureg.neutron**-1
 )
 
-# Run 1 transport coeff and measured TBR for overlay
-optimised_ratio = 0.015
+optimised_ratio = 0.01
 k_top = 12 * 1.45 * 8.9e-8 * ureg.m * ureg.s**-1
 k_wall = optimised_ratio * k_top
 
 baby_model = Model(
     radius=baby_radius,
     height=baby_height,
-    TBR=calculated_TBR * 1.25,
+    TBR=measured_TBR,
     neutron_rate=neutron_rate,
     irradiations=irradiations,
     k_top=k_top,
